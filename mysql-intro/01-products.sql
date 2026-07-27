02f0a4691f08d917b28de3ef1f6366ffcb7decd0
Gaetano Taormina <261669727+Gaetano-Taormina@users.noreply.github.com>
2026-07-27 11:30:00 +0200

    Create products table schema

---
+++ mysql-intro/01-products.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    quantity INT NOT NULL
+);
